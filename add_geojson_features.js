03cb3907665ac898f27a8ee1e56d23d72924bd87
Jerome <jerome.thai.87@gmail.com>
2015-03-05 03:49:32 -0800

    change color on mouse over

--- add_geojson_features.js
+++ add_geojson_features.js
@@ -6,9 +6,63 @@ fushia: #FF00FF GOOD
 */
 
 var myStyle = {
-    "color": "#00FFFF",
-    "weight": 5,
-    "opacity": 0.65
+    color: "#00FFFF",
+    weight: 5,
+    opacity: 0.5
+};
+
+
+var highlightStyle = {
+    color: '#FF00FF', 
+    weight: 5,
+    opacity: 1,
+};
+
+// got from this website
+// http://palewi.re/posts/2012/03/26/leaflet-recipe-hover-events-features-and-polygons/
+
+function onEachFeature(feature, layer) {
+    // Load the default style. 
+    //layer.setStyle(myStyle);
+    // Create a self-invoking function that passes in the layer
+    // and the properties associated with this particular record.
+    (function(layer, properties) {
+      // Create a mouseover event
+      layer.on("mouseover", function (e) {
+        // Change the style to the highlighted version
+        layer.setStyle(highlightStyle);});
+      // Create a mouseout event that undoes the mouseover changes
+      layer.on("mouseout", function (e) {
+        // Start by reverting the style back
+        layer.setStyle(myStyle); });
+      // Close the "anonymous" wrapper function, and call it while passing
+      // in the variables necessary to make the events work the way we want.
+    })(layer, feature.properties);
+
+    if (feature.properties) {
+    	var msg = ""
+    	if (feature.properties.user_id) {
+    	    msg = msg.concat("user_id: ")
+    	    msg = msg.concat(feature.properties.user_id)
+    	    msg = msg.concat("<br>")
+    	}
+        if (feature.properties.distance) {
+            msg = msg.concat("length: ")
+    	    msg = msg.concat(feature.properties.distance)
+    	    msg = msg.concat("<br>")
+        }
+        if (feature.properties.duration) {
+            msg = msg.concat("duration: ")
+    	    msg = msg.concat(feature.properties.duration)
+    	    msg = msg.concat("<br>")
+        }
+        if (feature.properties.number_points) {
+            msg = msg.concat("#points: ")
+    	    msg = msg.concat(feature.properties.number_points)
+    	    msg = msg.concat("<br>")
+        }
+        layer.bindPopup(msg);
+    }
 };
 
 
@@ -42,6 +96,6 @@ function popup(feature, layer) {
 
 
 L.geoJson(geojson_features, {
-    onEachFeature: popup,
+    onEachFeature: onEachFeature,
     style: myStyle
 }).addTo(map);
