e3d79f9e7f61eefa67f3d173adddb7969c700d3e
Jerome <jerome.thai.87@gmail.com>
2015-03-04 19:31:59 -0800

    filter data

--- data_import.py
+++ data_import.py
@@ -1,9 +1,12 @@
 import pandas as pd
 from utils import delta_time, distance_on_unit_sphere
 
+TOL = 1e-6
+
 __author__ = 'jeromethai'
 
 def add_raw_traj(line):
+    """create panda dataframe with trajectory of one user"""
     p = line.find('[')
     user_id = line[:p-1]
     rows = [] 
@@ -18,9 +21,7 @@ def add_raw_traj(line):
                 dist = 6373 * distance_on_unit_sphere(lat, lon, old_lat, old_lon)
                 # time in sec
                 deltatime = delta_time(old_time, time)
-            else: 
-                dist = None
-                deltatime = None
+            else: dist, deltatime = 0., 0.
             rows.append([lat, lon, time, dist, deltatime])
             old_lat, old_lon, old_time = lat, lon, time
 
@@ -31,19 +32,48 @@ def add_raw_traj(line):
 
 
 def csv_to_panda(filepath, add_traj):
+    """Import the whole csv into panda"""
     list_df = []
     with open(filepath) as f:
         for i, line in enumerate(f): list_df.append(add_traj(line))
     return pd.concat(list_df)
 
 
+def remove_static_points(df, deltatime):
+    """Remove consecutive points with same position within deltatime seconds"""
+    if len(df) <= 2: return df
+    rows, p1, p2 = [], 0, 1
+    while p1 < len(df):
+        rows.append(df.loc[p1].tolist())
+        time = 0
+        while p2 < len(df) and df.loc[p2]['dist_to_prev'] < TOL and df.loc[p2]['time_to_prev'] < deltatime:
+            time += df.loc[p2]['time_to_prev']
+            p2 += 1
+        if p1 < p2-1: rows.append(df.loc[p2-1].tolist()[:-1] + [time])
+        p1 = p2
+        p2 = p2+1
+    return pd.DataFrame(rows, columns = df.columns)
+
+
+def filter_data(df):
+    temp = []
+    for user_id in df.index.levels[0]:
+        print 'filter traj for', user_id
+        filtered_traj = remove_static_points(df.loc[user_id], 300)
+        filtered_traj['user_id'] = user_id
+        filtered_traj.set_index('user_id', append=True, inplace=True)
+        temp.append(filtered_traj.reorder_levels(['user_id', None]))
+    return pd.concat(temp)
+
+
 def main():
     filepath = 'data/stem_LA_sample_users_all_type_6_0902.csv'
     traj_data = csv_to_panda(filepath, add_raw_traj)
-    print traj_data
-    print traj_data.ix['196632997806448']
     print 'import complete'
-
+    filtered_data = filter_data(traj_data)
+    print traj_data.loc['035420522537597']
+    print filtered_data.loc['035420522537597']
+    
 
 if __name__ == "__main__":
     main()
--- tests/test_data_import.py
+++ tests/test_data_import.py
@@ -1,8 +1,9 @@
 import unittest
 import argparse
 import pandas as pd
-from data_import import add_raw_traj, csv_to_panda
+from data_import import add_raw_traj, csv_to_panda, remove_static_points
 import numpy as np
+from utils import delta_time, distance_on_unit_sphere
 
 __author__ = 'jeromethai'
 
@@ -13,30 +14,32 @@ class TestDataImport(unittest.TestCase):
         self.assertTrue(len(df) == 5)
         self.assertTrue(len(df.loc['23']) == 2)
         self.assertTrue(len(df.loc['24']) == 3)
+        val1 = 6373 * distance_on_unit_sphere(33.9, -8.4, 32.9, -7.4)
+        val2 = delta_time('2014-09-03 21:10:42-07:00', '2014-09-03 21:14:27-07:00')
 
         self.assertTrue(df.loc['23'].loc[0]['lat'] == 1.9)
         self.assertTrue(df.loc['23'].loc[0]['lon'] == -2.4)
         self.assertTrue(df.loc['23'].loc[0]['timeStamp'] == '2014-09-02 11:10:15-07:00')
-        self.assertTrue(np.isnan(df.loc['23'].loc[0]['dist_to_prev']))
-        self.assertTrue(np.isnan(df.loc['23'].loc[0]['time_to_prev']))
+        self.assertTrue(df.loc['23'].loc[0]['dist_to_prev'] == 0.)
+        self.assertTrue(df.loc['23'].loc[0]['time_to_prev'] == 0.)
 
         self.assertTrue(df.loc['23'].loc[1]['lat'] == 2.9)
         self.assertTrue(df.loc['23'].loc[1]['lon'] == -5.4)
         self.assertTrue(df.loc['23'].loc[1]['timeStamp'] == '2014-09-02 11:14:27-07:00')
-        self.assertFalse(np.isnan(df.loc['23'].loc[1]['dist_to_prev']))
-        self.assertFalse(np.isnan(df.loc['23'].loc[1]['time_to_prev']))
+        self.assertFalse(df.loc['23'].loc[1]['dist_to_prev'] == 0.)
+        self.assertFalse(df.loc['23'].loc[1]['time_to_prev'] == 0.)
 
         self.assertTrue(df.loc['24'].loc[0]['lat'] == 33.9)
         self.assertTrue(df.loc['24'].loc[0]['lon'] == -8.4)
         self.assertTrue(df.loc['24'].loc[0]['timeStamp'] == '2014-09-03 21:10:42-07:00')
-        self.assertTrue(np.isnan(df.loc['24'].loc[0]['dist_to_prev']))
-        self.assertTrue(np.isnan(df.loc['24'].loc[0]['time_to_prev']))
+        self.assertTrue(df.loc['24'].loc[0]['dist_to_prev'] == 0.)
+        self.assertTrue(df.loc['24'].loc[0]['time_to_prev'] == 0.)
 
         self.assertTrue(df.loc['24'].loc[1]['lat'] == 32.9)
         self.assertTrue(df.loc['24'].loc[1]['lon'] == -7.4)
         self.assertTrue(df.loc['24'].loc[1]['timeStamp'] == '2014-09-03 21:14:27-07:00')
-        self.assertFalse(np.isnan(df.loc['24'].loc[1]['dist_to_prev']))
-        self.assertFalse(np.isnan(df.loc['24'].loc[1]['time_to_prev']))
+        self.assertTrue(df.loc['24'].loc[1]['dist_to_prev'] == val1)
+        self.assertTrue(df.loc['24'].loc[1]['time_to_prev'] == val2)
 
 
     def test_add_raw_traj(self):
@@ -45,11 +48,30 @@ class TestDataImport(unittest.TestCase):
         df = pd.concat([add_raw_traj(line1), add_raw_traj(line2)])
         self.assert_contents(df) 
     
+
     def test_add_csv_to_panda(self):
         df = csv_to_panda('tests/sample_data.csv', add_raw_traj)
         self.assert_contents(df)
-        
-		
+
+
+    def test_remove_static_points(self):
+
+        data = [[4.2, 0.0, 0.0], [6.4, 0.0, 100], [3.2, 0.0, 280], [7.8, 0.0, 2]]
+        columns = ['data', 'dist_to_prev', 'time_to_prev']
+        df = pd.DataFrame(data, index = range(4), columns = columns)
+        df = remove_static_points(df, 300)
+        self.assertTrue(len(df) == 2)
+        self.assertTrue(df.loc[0]['data'] == 4.2)
+        self.assertTrue(df.loc[1]['data'] == 7.8)
+        self.assertTrue(df.loc[0]['dist_to_prev'] == 0.)
+        self.assertTrue(df.loc[1]['dist_to_prev'] == 0.)
+        self.assertTrue(df.loc[0]['time_to_prev'] == 0.)
+        self.assertTrue(df.loc[1]['time_to_prev'] == 382)
+
+        data = [[4.2, 0.0, 0.0], [6.4, 0.0, 350], [3.2, 1.0, 280], [7.8, 2.0, 2]]
+        df = pd.DataFrame(data, index = range(4), columns = columns)
+        df = remove_static_points(df, 300)
+        self.assertTrue(len(df) == 4)
 
 if __name__ == '__main__':
 	unittest.main()
